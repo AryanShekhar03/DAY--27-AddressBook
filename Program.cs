93a406c34dff8fb50b2b7bc1ae32b58596cbbbfb
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2022-01-17 13:43:16 +0530

    Aaryan added ReadandWrite json

--- Program.cs
+++ Program.cs
@@ -41,8 +41,7 @@ namespace AddressBookSystem
                     key = Convert.ToInt32(Console.ReadLine());
                 }
                 AddressBook.AddTo(book);
-                AddressBook.CsvSerialise();
-                AddressBook.CsvDeserialise();
+                AddressBook.JsonSerializeAddressBook();
                 numberBook++;
             }
         }
